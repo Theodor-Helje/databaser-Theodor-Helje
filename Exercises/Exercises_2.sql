9eeaa963b92907642b96b3ff102312ee1dab82de
Theodor Helje <Theodor.Helje@proton.me>
2026-05-02 16:34:02 +0200

    work on exercises 2.2

--- Exercises/Exercises_2.sql
+++ Exercises/Exercises_2.sql
@@ -1,3 +1,23 @@
 USE everyloop;
 GO
 
+
+--a
+SELECT
+    Period,
+    MIN(Number) AS [From],
+    MAX(Number) AS [to],
+    COALESCE(ROUND(AVG(Stableisotopes), 2), '-') AS [Average stable isotopes],
+    STRING_AGG(Symbol, ',') AS [Symbols]
+FROM everyloop.dbo.Elements
+GROUP BY Period;
+
+
+SELECT
+    Region,
+    Country,
+    City,
+    COUNT(Id) AS Customers
+FROM everyloop.company.customers
+WHERE COUNT(Id) >= 2
+GROUP BY City;
