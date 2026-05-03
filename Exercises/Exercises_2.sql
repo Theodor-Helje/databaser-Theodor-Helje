23fc6fc5f233e3f4b94327461e923b996b42221e
Theodor Helje <Theodor.Helje@proton.me>
2026-05-03 15:09:36 +0200

    completed exercide 2.c

--- Exercises/Exercises_2.sql
+++ Exercises/Exercises_2.sql
@@ -28,3 +28,16 @@ HAVING COUNT(Id) >= 2;
 
 
 --c
+DECLARE @text VARCHAR(MAX) = ''
+
+SELECT @text = @text +
+    'Säsong ' + CAST([Season] AS VARCHAR) +
+    ' sändes från ' + FORMAT(MIN([Original air date]), 'MMMM', 'sv') +
+    ' till ' + FORMAT(MAX([Original air date]), 'MMMM yyyy', 'sv') +
+    '. Totalt sändes ' + CAST(COUNT([EpisodeInSeason]) AS VARCHAR)  +
+    ' avsnitt, som i genomsnitt sågs av ' + CAST(ROUND(AVG([U.S. viewers(millions)]), 2) AS VARCHAR) +
+    ' miljoner människor i USA' + CHAR(13) + CHAR(10)
+FROM everyloop.dbo.GameOfThrones
+GROUP BY [Season]
+
+PRINT @text
