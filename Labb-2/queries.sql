8c551bef7a1e10f5fad7659e96ce17a5b299a841
Theodor Helje <Theodor.Helje@proton.me>
2026-05-18 14:18:00 +0200

    created more tables

--- Labb-2/queries.sql
+++ Labb-2/queries.sql
@@ -2,12 +2,15 @@ USE bokhandel;
 GO
 
 
+DROP TABLE IF EXISTS [bokhandel].[dbo].[OrderDetaljer];
+DROP TABLE IF EXISTS [bokhandel].[dbo].[Ordrar];
+DROP TABLE IF EXISTS [bokhandel].[dbo].[Anställda];
 DROP TABLE IF EXISTS [bokhandel].[dbo].[LagerSaldo];
 DROP TABLE IF EXISTS [bokhandel].[dbo].[Böcker];
+DROP TABLE IF EXISTS [bokhandel].[dbo].[Butiker];
+DROP TABLE IF EXISTS [bokhandel].[dbo].[Kunder];
 DROP TABLE IF EXISTS [bokhandel].[dbo].[Förlag];
 DROP TABLE IF EXISTS [bokhandel].[dbo].[Författare];
-DROP TABLE IF EXISTS [bokhandel].[dbo].[Anställda];
-DROP TABLE IF EXISTS [bokhandel].[dbo].[Butiker];
 
 
 
@@ -19,9 +22,26 @@ CREATE TABLE Författare(
 );
 
 
-CREATE TABLE Förlag(
+CREATE TABLE Förlag( --övr
     [ID] INT IDENTITY(1, 1) PRIMARY KEY,
     [Namn] NVARCHAR(MAX) NOT NULL
+);
+
+
+CREATE TABLE Kunder( --övr
+    [ID] INT IDENTITY(1, 1) PRIMARY KEY, --identity tills kund-id-standard är bestämd
+    [UserName] NVARCHAR(MAX) NOT NULL,
+    [EmailAdress] NVARCHAR(MAX) CHECK ([EmailAdress] LIKE '%@%')
+);
+
+
+CREATE TABLE Butiker(
+    [ID] INT IDENTITY(1, 1) PRIMARY KEY,
+    [Butiksnamn] VARCHAR(MAX) NOT NULL,
+    [Land] VARCHAR(MAX) NOT NULL,
+    [STAD] VARCHAR NOT NULL,
+    [Adress] VARCHAR(MAX) NOT NULL,
+    [Postnummer] VARCHAR(MAX) NOT NULL
 );
 
 
@@ -36,16 +56,6 @@ CREATE TABLE Böcker(
 );
 
 
-CREATE TABLE Butiker(
-    [ID] INT IDENTITY(1, 1) PRIMARY KEY,
-    [Butiksnamn] VARCHAR(MAX) NOT NULL,
-    [Land] VARCHAR(MAX) NOT NULL,
-    [STAD] VARCHAR NOT NULL,
-    [Adress] VARCHAR(MAX) NOT NULL,
-    [Postnummer] VARCHAR(MAX) NOT NULL
-);
-
-
 CREATE TABLE LagerSaldo(
     [ButikId] INT FOREIGN KEY REFERENCES Butiker(ID),
     [ISBN] CHAR(13) FOREIGN KEY REFERENCES Böcker(ISBN13),
@@ -54,10 +64,28 @@ CREATE TABLE LagerSaldo(
 );
 
 
-CREATE TABLE Anställda(
+CREATE TABLE Anställda( --övr
     [ID] INT IDENTITY(1, 1) PRIMARY KEY,
     [Förnamn] NVARCHAR(MAX) NOT NULL,
     [Efternamn] NVARCHAR(MAX) NOT NULL,
     [Titel] NVARCHAR(MAX) NOT NULL,
     [ButikId] INT FOREIGN KEY REFERENCES Butiker(ID)
+);
+
+
+CREATE TABLE Ordrar( --övr
+    [ID] INT IDENTITY(1, 1) PRIMARY KEY,
+    [KundID] INT FOREIGN KEY REFERENCES Kunder(ID),
+    [AnställdID] INT FOREIGN KEY REFERENCES Anställda(ID),
+    [Adress] NVARCHAR(MAX) NOT NULL,
+    [Beställt] DATE NOT NULL,
+    [Skickat] DATE
+);
+
+CREATE TABLE OrderDetaljer( --övr
+    [OrderID] INT FOREIGN KEY REFERENCES Ordrar(ID),
+    [BokID] CHAR(13) FOREIGN KEY REFERENCES Böcker(ISBN13),
+    [Antal] INT NOT NULL CHECK ([Antal] > 0),
+    [Kostnad] DECIMAL(10, 2), --lägg till automatiskt i insert query sen
+    PRIMARY KEY ([OrderID], [BokID])
 );
